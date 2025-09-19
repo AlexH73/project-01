e43b6af8192973731e18691457b44d4bcef82fa8
Alexander Hermann <ewebotah@gmail.com>
2025-09-19 18:20:34 +0200

    feat(global):
    Enabled the Bootstrap library

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,15 @@ function App() {
               codeUrl="https://github.com/AlexH73/frontend-pro/blob/main/lessons/lesson_05/theory.md"
               urlAnchor="Theory"
             />
-            <div className="components-grid">Loading...</div>
+            <div className="components-grid">
+              <div
+                className="spinner-border"
+                style={{ width: "3rem", height: "3rem" }}
+                role="status"
+              >
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
           </section>
 
           <section id="homework-05" className="section">
